32748f467e3545701c40c45a1cf0033ef533e5a3
Tasneem <tasneem_4890@limu.edu.ly>
2026-06-28 14:02:55 +0200

    fix(terraform): explicitly enable oidc_issuer to match azure state

--- main.tf
+++ main.tf
@@ -20,6 +20,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "cloudscale"
 
+  oidc_issuer_enabled = true
+  
   default_node_pool {
     name       = "default"
     node_count = var.node_count
